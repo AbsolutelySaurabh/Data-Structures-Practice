f3f557f11d11a631f05007f0e7db7c3035ba72bc
= <=>
2017-09-03 16:39:34 +0530

    All methods implemented

--- LinkedList/DS/src/LinkedList/LinkedList.java
+++ LinkedList/DS/src/LinkedList/LinkedList.java
@@ -10,6 +10,20 @@ public class LinkedList {
 		Node head = takeInput();
 		printOutput(head);
 		
+		Node tail = findTail(head);
+		System.out.println("The tail is:  "+ tail.data);
+		
+		System.out.println("the length is: "+ length(head, 0));
+		
+//		if(checkPallindrome(head)){
+//			
+//			System.out.println("The list is a Pallindrome");
+//		}else{
+//			
+//			System.out.println("The is Not Pallindrome");
+//		}
+		
+		
 		System.out.println("Enter the position you want to insert at: ");
 		int insertPosition = s.nextInt();
 		Node newHeadInserted = insertAtIth(head, head, insertPosition, 0);
@@ -19,14 +33,20 @@ public class LinkedList {
 		System.out.println("Enter the positions you want to swap the nodes at: ");
 		int swap_pos_1 = s.nextInt();
 		int swap_pos_2 = s.nextInt();
-		Node newHeadSwapped = swapNodes(head, temp, swap_pos_1, swap_pos_2);
+		Node newHeadSwapped = swapNodes(head, swap_pos_1, swap_pos_2);
+		System.out.println("After being swapped: ");
+		printOutput(newHeadSwapped);
 		
+		System.out.println("After reversing the List:  ");
+		Node rev_head = reverse(head, head, head.next);
+		printOutput(rev_head);
+
 		System.out.println("Enter the position of the node you want to delete");
 		int deletePosition = s.nextInt();
 		Node newHeadDeleted = deleteAtIth(head,head, deletePosition, 0);
 		System.out.println("LinkedList after deletion: ");
 		printOutput(newHeadDeleted);
-			
+//			
 	}
 	
 	public static void printOutput(Node head){
@@ -38,28 +58,104 @@ public class LinkedList {
 		printOutput(head.next);	
 	}
 	
-	public static Node swapNodes(Node head, Node temp, int swap_pos_1, int swap_pos_2){
+	public static int length(Node head, int count){
 		
-		for(int i=0;i<swap_pos_1-1;i++){
+		if(head==null){
+			return count;
+		}
+		return length(head.next, ++count);	
+	}
+	
+	
+	public static Node findTail(Node head){
+		
+		if(head.next == null){
+			return head;
+		}
+		return findTail(head.next);
+		
+	}
+	
+	public static boolean checkPallindrome(Node head){
+
+		int length = length(head, 0);
+		Node first_part_head = head, second_part_head = head;
+		if(length%2==0){
 			
-			temp = temp.next;
+			for(int i=0; i<length-2;i++){
+				second_part_head = second_part_head.next;
+			}			
 		}
 		
-		Node swap_2_temp = head;
-		Node swap_2_prev_temp = head;
+		Node rev_head = reverse(head, second_part_head, second_part_head.next);
+		
+		if(checkPallindromeAfterRev(first_part_head, rev_head)){
+			
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public static boolean checkPallindromeAfterRev(Node first_part_head, Node rev_head){
+		
+		if(rev_head == null || first_part_head ==null){
+			return true;
+		}
+		
+		if(first_part_head.data!= rev_head.data){
+			return false;
+		}else{
+			
+			return checkPallindromeAfterRev(first_part_head.next, rev_head.next);
+		}
+		
+	}
+	
+	public static Node reverse(Node head, Node prev, Node present){
+		
+		if(prev.next==null || present==null){
+			head.next=null;
+			return prev;
+		}
+		
+		Node temp_prev = present;
+		Node temp_present = present.next;
+		present.next = prev;
+		return reverse(head, temp_prev, temp_present);
+	}
+	
+	public static Node swapNodes(Node head, int swap_pos_1, int swap_pos_2){
+		
+		Node swap_1 = head, swap_2 = head;
+		Node prev_1 = head;
+		Node prev_2 = head;
+		
+		for(int i=0;i<swap_pos_1;i++){
+			
+			swap_1 = swap_1.next;
+		}
 		
 		for(int i=0;i<swap_pos_2;i++){
-			swap_2_temp = swap_2_temp.next;
+			
+			swap_2 = swap_2.next;
+		}
+		
+		
+		for(int i=0;i<swap_pos_1-1;i++){
+			prev_1 = prev_1.next;
 		}
 		
 		for(int i=0;i<swap_pos_2-1 ;i++){
 			
-			swap_2_prev_temp = swap_2_prev_temp.next;
+			prev_2 = prev_2.next;
 		}
 		
-		Node swap_1_next_temp = temp;
-		swap_2_prev_temp.next = temp.next.next;
-		temp.next = swap_2_temp;
+		Node temp_2 = swap_2.next;
+		prev_1.next = swap_2;
+		swap_2.next = swap_1.next;
+		swap_1.next = temp_2;
+		prev_2.next = swap_1;
 	
 		return head;
 	}
