a02a6e20750f3a39470c520da37e42dff545aba8
absolutelysaurabh <absolutelysaurabh03@gmail.com>
2018-02-28 00:18:11 +0530

    practices string algos

---
+++ Algorithms/strings/AnagramsDelete.java
@@ -0,0 +1,43 @@
+package strings;
+
+import java.util.Scanner;
+
+public class AnagramsDelete {
+	
+	//take arrays of length 26 as the maxn unique character in abcde is 26 no.
+	
+	public static int countAnagrams(String s1, String s2) {
+		
+	//maintain two count1, count2 arrays, containign the frequency of each character in string
+	int[] count1 = new int[26], count2 = new int[26];
+	//frequency of each character in s1
+	for(int i=0;i<s1.length();i++) {
+		count1[s1.charAt(i) - 'a']++;;
+	}
+	
+	for(int i=0;i<s2.length();i++) {
+		count2[s2.charAt(i) - 'a']++;
+	}
+	
+	int result=0;
+	
+	//not calulate the result
+	for(int i=0;i<26;i++) {
+		result+=Math.abs((count1[i] - count2[i]));
+	}
+	return result;
+	
+	}
+
+	public static void main(String[] args) {
+		
+		String s1, s2;
+		Scanner s = new Scanner(System.in);
+		s1 = s.nextLine();
+		s2 = s.nextLine();
+		
+		System.out.println(countAnagrams(s1, s2));
+
+	}
+
+}
---
+++ Algorithms/strings/RansomeNote.java
@@ -0,0 +1,89 @@
+package strings;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class RansomeNote {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		int m = s.nextInt();
+		int n = s.nextInt();
+		
+		String magazine = s.nextLine();
+		System.out.println(magazine.length() + "...");
+		String[] arr_m = magazine.split(" ");
+
+		String note = s.nextLine();
+		System.out.println(note.length()+ "...");
+		String[] arr_n = note.split(" ");
+		
+//		for(int i=0;i<arr_m.length;i++) {
+//			System.out.print(arr_m[i] + " ");
+//		}
+//		System.out.println(" ");
+//		for(int i=0;i<arr_n.length;i++) {
+//			System.out.print(arr_n[i] + " ");
+//		}
+		
+		//System.out.println(arr_m.length + " -> " + arr_n.length);
+		
+		HashMap<String, Integer> map_magazine = new HashMap(), map_note = new HashMap();
+		//input in hashmap
+		for(int i=0;i<arr_m.length;i++) {
+			if(map_magazine.containsKey(arr_m[i])) {
+				map_magazine.put(arr_m[i], map_magazine.get(arr_m[i]) + 1);
+				//System.out.print("map_magazine "+ arr_m[i]);
+			}else {
+				map_magazine.put(arr_m[i], 0);
+			}
+		}
+		
+		for(int i=0;i<arr_n.length;i++) {
+			if(map_note.containsKey(arr_n[i])) {
+				map_note.put(arr_n[i], map_note.get(arr_n[i]) + 1);
+				//System.out.print("map_note "+ arr_n[i]);
+			}else {
+				map_note.put(arr_n[i], 0);
+			}
+		}
+//		
+//		for(String key: map_magazine.keySet()) {
+//			
+//			System.out.print(key + "->" + map_magazine.get(key));
+//		}
+//		System.out.println(" ");
+//
+//		for(String key: map_note.keySet()) {
+//			
+//			System.out.print(key + "->" + map_note.get(key));
+//		}
+//		
+		
+		int flag = 1;
+		
+		//calculate the frequency of each word
+		for(String key: map_note.keySet()) {
+			
+			if(map_magazine.containsKey(key)) {
+				if(map_note.get(key)!=map_magazine.get(key)) {
+					//flag = 0;
+					flag = 0;
+					break;
+				}
+			}else {
+				flag = 0;
+				break;
+			}
+		}
+		
+		if(flag == 1) {
+			System.out.println("Yes");
+		}else {
+			System.out.println("No");
+		}
+			
+	}
+
+}
